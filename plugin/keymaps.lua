e8255c9c60d999b8065813935e3802ab35fa3c8e
ge3224 <ge3224@gmail.com>
2025-02-22 08:16:02 -0600

    add special key map for repetitive task

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -30,3 +30,33 @@ vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<CR>", { silent = true })
 vim.keymap.set("n", "<leader>zc", function()
   vim.cmd [[call system("rm -rdf ~/.local/state/nvim/swap")]]
 end, { silent = true })
+
+-- Function to generate the numbered links
+local function generate_numbered_links()
+  -- Prompt for input parameters
+  local x = vim.fn.input "Enter chapter number: "
+  local count = tonumber(vim.fn.input "Enter number of verses: ")
+  local c_id = tonumber(vim.fn.input "Enter the chapter ID: ")
+  local v_id = tonumber(vim.fn.input "Enter the initial verse ID: ")
+
+  -- Get current cursor position
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  -- Generate the lines
+  local lines = {}
+  for i = 1, count do
+    local line = string.format("### [%s:%d](https://wol.jw.org/en/wol/dx/r1/lp-e/%d/%d)", x, i, c_id, v_id + i - 1)
+    table.insert(lines, line)
+    -- Add empty line after each entry except the last one
+    if i < count then
+      table.insert(lines, "")
+    end
+  end
+
+  -- Insert the lines at current cursor position
+  vim.api.nvim_buf_set_lines(0, row - 1, row - 1, false, lines)
+end
+
+-- Create the keymapping (adjust the key combination as needed)
+vim.keymap.set("n", "<leader>bv", function()
+  generate_numbered_links()
+end, { noremap = true, desc = "Generate numbered links" })

b46760447ab8c831de120c5f15cf0940436fd715
ge3224 <ge3224@gmail.com>
2025-05-17 08:43:35 -0500

    test(lsp): trying ai suggestion for ts_ls & denols conflicts

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -208,33 +208,26 @@ return {
       -- But for many setups, the LSP (`tsserver`) will work just fine
       -- tsserver = {},
       --
+      -- https://docs.deno.com/runtime/getting_started/setup_your_environment/#kickstart.nvim-and-mason-lsp
       ts_ls = {
         root_dir = require("lspconfig").util.root_pattern { "package.json", "tsconfig.json" },
         single_file_support = false,
         settings = {},
       },
-      -- ts_ls = {
-      --   filetypes = { "typescript", "typescriptreact" },
-      --   root_dir = function(fname)
-      --     -- Check if deno config exists
-      --     local deno_root = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")(fname)
-      --     if deno_root then
-      --       return nil -- Don't attach tsserver if deno config exists
-      --     end
-      --     -- Otherwise, look for package.json
-      --     return require("lspconfig.util").root_pattern "package.json"(fname)
-      --   end,
-      --   single_file_support = false,
-      -- },
       denols = {
-        root_dir = require("lspconfig").util.root_pattern { "deno.json", "deno.jsonc" },
+        function()
+          -- Only attach if a Deno root marker is found in the upward search.
+          local deno_root = vim.fs.find({ "deno.json", "deno.jsonc" }, { upward = true })[1]
+          if deno_root then
+            return vim.fn.fnamemodify(deno_root, ":h") -- Return the directory containing the deno config
+          else
+            return nil
+          end
+        end,
+        -- root_dir = require("lspconfig").util.root_pattern { "deno.json", "deno.jsonc" },
         single_file_support = false,
         settings = {},
       },
-      -- denols = {
-      --   filetypes = { "typescript", "typescriptreact" },
-      --   root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc"),
-      -- },
       lua_ls = {
         -- cmd = {...},
         -- filetypes = { ...},

c7471304668e3eb917b185307280242d6589bb2c
ge3224 <ge3224@gmail.com>
2025-12-25 15:28:13 -0600

    feat: add mapping to copy current cursor position

--- plugin/keymaps.lua
+++ plugin/keymaps.lua
@@ -13,3 +13,13 @@ vim.keymap.set('n', 'N', 'Nzzzv', { silent = true })
 -- Navigate quickfix list
 vim.keymap.set('n', 'gn', '<cmd>cn<CR>', { silent = true })
 vim.keymap.set('n', 'gp', '<cmd>cN<CR>', { silent = true })
+
+-- Copy cursor position to clipboard (filepath:line:col)
+vim.keymap.set('n', '<leader>yl', function()
+  local filepath = vim.fn.expand '%:p'
+  local line = vim.fn.line '.'
+  local col = vim.fn.col '.'
+  local position = string.format('%s:%d:%d', filepath, line, col)
+  vim.fn.setreg('+', position)
+  vim.notify('Copied: ' .. position, vim.log.levels.INFO)
+end, { desc = 'Copy cursor position to clipboard' })

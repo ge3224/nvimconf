2b7073188420d8a26bea586cf267e6ec22407d05
ge3224 <ge3224@gmail.com>
2026-05-15 08:20:07 -0500

    fix: textwidth consistency

--- plugin/autocommands.lua
+++ plugin/autocommands.lua
@@ -1,4 +1,11 @@
 vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function()
+    vim.opt_local.textwidth = 80
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
   group = vim.api.nvim_create_augroup('treesitter', {
     clear = true,
   }),
